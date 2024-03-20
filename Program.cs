217a90beb48dfd7577dc2a422baa375ab3004f1b
DESKTOP-A37MTN2\csi22-jzarcia <jose.manuel.zarco.garcia@gmail.com>
2024-03-20 10:49:04 +0100

    no message

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ejercicioFichero3
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string fichero = "C:\\Users\\csi22-jzarcia\\Desktop\\textoACambiar.txt";
+
+
+            try
+            {
+                Console.WriteLine("En que linea deseas escribir: ");
+                int numeroLinea = Convert.ToInt32(Console.ReadLine());
+
+
+                string[] lineas = File.ReadAllLines(fichero);
+                if (numeroLinea >= 1 && numeroLinea <= lineas.Length)
+                {
+                    Console.WriteLine("Que desea escribir: ");
+                    string nuevoTexto = Console.ReadLine();
+
+                    lineas[numeroLinea - 1] = nuevoTexto;
+
+                    File.WriteAllLines(fichero, lineas);
+                }
+                else
+                {
+                    Console.WriteLine("La linea en la que desea escribir esta fuera de rango");
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error al leer o escribir en el archivo: " + e.Message);
+            }
+
+            
+
+        }
+
+    }
+
+}
